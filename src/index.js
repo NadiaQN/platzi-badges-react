eada57156ebd6aa73f2935fe37fe3d4e60e111d9
nadiaqn <quezada.nadia@gmail.com>
2020-01-16 23:49:01 -0300

    clase jsx

--- src/index.js
+++ src/index.js
@@ -8,9 +8,16 @@ container.appendChild(element)**/
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const element = <h1>Hello, Platzi Badges from React!!</h1>
 
+//const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a platzi')
+//const name = 'Nadia';
+//const element = React.createElement('h1', {}, `Hola, soy ${name}`)
+//const jsx = <h1>hola soy {name}</h1>
+
+const jsx = <div>
+    <h1>Hola, soy Nadia</h1>
+    <p>Estoy aprendiendo React</p>
+</div>
 const container = document.getElementById('app')
-
 // render recibe 2 argumentos, qué y donde
-ReactDom.render(element, container)
+ReactDom.render(jsx, container)
